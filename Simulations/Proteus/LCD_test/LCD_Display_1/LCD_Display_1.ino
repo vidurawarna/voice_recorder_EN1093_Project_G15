fe756b90474b8766950efd2e1947b41ecc29f61b
Nifla Nasvi <niflanasvi2000@gmail.com>
2021-04-20 16:45:37 +0530

    seperated functions for lcd arduino simulations

--- Simulations/Proteus/LCD_test/LCD_Display_1/LCD_Display_1.ino
+++ Simulations/Proteus/LCD_test/LCD_Display_1/LCD_Display_1.ino
@@ -4,7 +4,8 @@
 const int ROW_NUM = 4; //four rows
 const int COLUMN_NUM = 3; //three columns
 
-LiquidCrystal lcd(2,1,13,12,11,10);
+const int rs=2,e=1,d4=13,d5=12,d6=11,d7=10;
+LiquidCrystal lcd(rs,e,d4,d5,d6,d7);
 
 char keys[ROW_NUM][COLUMN_NUM] = {
   {'1','2','3'},
@@ -20,55 +21,54 @@ Keypad keypad = Keypad( makeKeymap(keys), pin_rows, pin_column, ROW_NUM, COLUMN_
 
 void setup() {
   lcd.begin(16,2);
+  
   lcd.setCursor(5,0);
   lcd.print("Welcome");
   delay(1000);
   lcd.clear();
+  lcd.print("Play list");
+  delay(1000);
+  lcd.clear();
+  play_list();
 }
 
-String playList[10]={"Track1","Track2","Track3","Track4","Track5","Track6","Track7","Track8","Track9","Track10"};
-void loop(){
-  lcd.print("Select one to play");
-  delay(500);
-  lcd.clear();
+String playList[10]={"Audio 1","Audio 2","Audio 3","Audio 4","Audio 5","Audio 6","Audio 7","Audio 8","Audio 9","Audio 10"};
+
+void play_list(){
   for (int i =0; i<=9; i++){
+    lcd.setCursor(0,0);
+    lcd.print("Track ");
     lcd.print(i+1);
-    lcd.print(" ");
+    delay(10);
+    lcd.setCursor(0,2);
     lcd.print(playList[i]);
-    delay(500);
-    lcd.clear();
-  }
-  lcd.print("Select!");
-  delay(500);
-  lcd.clear();
-  /*boolean NO_KEY = true;
-  int key = keypad.getKey();
-  while(key == NO_KEY){
-    lcd.print("Select one");
-    delay(1000);
-  }
-  if(key !=NO_KEY){
-    lcd.print(key);
     delay(1000);
     lcd.clear();
-    }
-  }*/
+  }
+} 
+
+void play_audio(){
   int key = keypad.getKey();
-  /*lcd.print(key);
-  delay(500);
-  lcd.clear();*/
   while (key ==0){
     key = keypad.getKey();
   }
   if(key !=0){
-    lcd.print(key-48);
+    //lcd.print(key-48);
     lcd.print(" ");
     lcd.print(playList[key-49]);
     lcd.print(" is playing");
-    delay(1000);
+    delay(3000);
     lcd.clear();
   }
+}
+
+void loop(){
+  lcd.print("Select one to play!");
+  delay(1000);
+  lcd.clear();
+  play_audio();
+  
   lcd.print("next");
-  delay(500);
+  delay(1000);
   lcd.clear();
 }
