de4d6b5d6fc9899fc9a844dd4a8711f0738c3840
nujaadeen <nushad9432@gmail.com>
2021-05-22 14:22:46 +0530

    dac using filter files added

---
+++ Simulations/Proteus/DAC with filter/dac_ex1/dac_ex1.ino
@@ -0,0 +1,48 @@
+
+
+void setup() {
+  ADCSRA |= (1 << ADPS2);
+  ADCSRA &= ~(1 << ADPS1);
+  ADCSRA &= ~(1 << ADPS0);
+// Serial.begin(115200); //for debugging
+  pinMode(10, OUTPUT); //pin used for analog voltage value
+  setPwmFrequency(10,1); //function for setting PWM frequency
+
+}
+
+void loop() {
+  int temp = map(analogRead(A0),0,1023,0,255);
+//  Serial.println(temp);
+  analogWrite(10,temp);
+}
+
+void setPwmFrequency(int pin, int divisor) {
+  byte mode;
+  if(pin == 5 || pin == 6 || pin == 9 || pin == 10) {
+    switch(divisor) {
+      case 1: mode = 0x01; break;
+      case 8: mode = 0x02; break;
+      case 64: mode = 0x03; break;
+      case 256: mode = 0x04; break;
+      case 1024: mode = 0x05; break;
+      default: return;
+    }
+    if(pin == 5 || pin == 6) {
+      TCCR0B = TCCR0B & 0b11111000 | mode;
+    } else {
+      TCCR1B = TCCR1B & 0b11111000 | mode;
+    }
+  } else if(pin == 3 || pin == 11) {
+    switch(divisor) {
+      case 1: mode = 0x01; break;
+      case 8: mode = 0x02; break;
+      case 32: mode = 0x03; break;
+      case 64: mode = 0x04; break;
+      case 128: mode = 0x05; break;
+      case 256: mode = 0x06; break;
+      case 1024: mode = 0x07; break;
+      default: return;
+    }
+    TCCR2B = TCCR2B & 0b11111000 | mode;
+  }
+}
--- Simulations/Proteus/Sound_output/Speaker/Speaker.ino
+++ Simulations/Proteus/Sound_output/Speaker/Speaker.ino
@@ -6,16 +6,12 @@ void setup() {
 }
 
 void loop() {
-<<<<<<< HEAD
   /*for (int t=0; t<100; t++){
     PORTD = sine[t];
     delay(10);*/
     int t = analogRead(A0);
     PORTD = t;
     delayMicroseconds(8);
-  }
-=======
     PORTD = analogRead(A0);
     delayMicroseconds(1);
 }
->>>>>>> cd4e81e2fb0b81f9a873a78d6e9de7aff86958d0
