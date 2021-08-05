89138eb9747b9867dc0309f58dd84b9f9ad262da
Vidura Munasinghe <vidurawarna99@gmail.com>
2021-08-05 13:11:45 +0530

    OLED screen is added to the project as the display

---
+++ AVR/Adding_OLED/Voice_Recorder/Voice_Recorder/Debug/main.d
@@ -0,0 +1,287 @@
+main.d main.o: .././main.cpp \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.6.364\include/avr/iom328p.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
+ ../SD/src/SD.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdbool.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\pgmspace.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\ctype.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\eeprom.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h \
+ ../SD/src/utility/SdFat.h ../SD/src/utility/Sd2Card.h \
+ ../SD/src/utility/Sd2PinMap.h ../SD/src/utility/SdInfo.h \
+ ../SD/src/utility/FatStructs.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ ../SD/src/utility/SdFatUtil.h ../SPI/src/SPI.h ../Wire/src/Wire.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ ../SSD1306Ascii/src/SSD1306Ascii.h ../SSD1306Ascii/src/SSD1306init.h \
+ ../SSD1306Ascii/src/fonts/allFonts.h \
+ ../SSD1306Ascii/src/fonts/Adafruit5x7.h \
+ ../SSD1306Ascii/src/fonts/font5x7.h ../SSD1306Ascii/src/fonts/lcd5x7.h \
+ ../SSD1306Ascii/src/fonts/Stang5x7.h \
+ ../SSD1306Ascii/src/fonts/X11fixed7x14.h \
+ ../SSD1306Ascii/src/fonts/X11fixed7x14B.h \
+ ../SSD1306Ascii/src/fonts/ZevvPeep8x16.h \
+ ../SSD1306Ascii/src/fonts/System5x7.h \
+ ../SSD1306Ascii/src/fonts/SystemFont5x7.h \
+ ../SSD1306Ascii/src/fonts/Iain5x7.h ../SSD1306Ascii/src/fonts/Arial14.h \
+ ../SSD1306Ascii/src/fonts/Arial_bold_14.h \
+ ../SSD1306Ascii/src/fonts/Corsiva_12.h \
+ ../SSD1306Ascii/src/fonts/Verdana_digits_24.h \
+ ../SSD1306Ascii/src/fonts/Callibri10.h \
+ ../SSD1306Ascii/src/fonts/Callibri11.h \
+ ../SSD1306Ascii/src/fonts/Callibri11_bold.h \
+ ../SSD1306Ascii/src/fonts/Callibri11_italic.h \
+ ../SSD1306Ascii/src/fonts/Callibri14.h \
+ ../SSD1306Ascii/src/fonts/Callibri15.h \
+ ../SSD1306Ascii/src/fonts/Cooper19.h \
+ ../SSD1306Ascii/src/fonts/Cooper21.h \
+ ../SSD1306Ascii/src/fonts/Cooper26.h \
+ ../SSD1306Ascii/src/fonts/TimesNewRoman13.h \
+ ../SSD1306Ascii/src/fonts/TimesNewRoman13_italic.h \
+ ../SSD1306Ascii/src/fonts/TimesNewRoman16.h \
+ ../SSD1306Ascii/src/fonts/TimesNewRoman16_bold.h \
+ ../SSD1306Ascii/src/fonts/TimesNewRoman16_italic.h \
+ ../SSD1306Ascii/src/fonts/Verdana12.h \
+ ../SSD1306Ascii/src/fonts/Verdana12_bold.h \
+ ../SSD1306Ascii/src/fonts/Verdana12_italic.h \
+ ../SSD1306Ascii/src/fonts/Roosewood22.h \
+ ../SSD1306Ascii/src/fonts/Roosewood26.h \
+ ../SSD1306Ascii/src/fonts/fixednums7x15.h \
+ ../SSD1306Ascii/src/fonts/fixednums8x16.h \
+ ../SSD1306Ascii/src/fonts/fixednums15x31.h \
+ ../SSD1306Ascii/src/fonts/CalBlk36.h \
+ ../SSD1306Ascii/src/fonts/CalLite24.h \
+ ../SSD1306Ascii/src/fonts/lcdnums12x16.h \
+ ../SSD1306Ascii/src/fonts/lcdnums14x24.h \
+ ../SSD1306Ascii/src/fonts/fixed_bold10x15.h \
+ ../SSD1306Ascii/src/fonts/Wendy3x5.h \
+ ../SSD1306Ascii/src/fonts/newbasic3x5.h \
+ ../SSD1306Ascii/src/fonts/font8x8.h \
+ ../SSD1306Ascii/src/fonts/cp437font8x8.h \
+ ../SSD1306Ascii/src/fonts/utf8font10x16.h \
+ ../SSD1306Ascii/src/SSD1306AsciiAvrI2c.h \
+ ../SSD1306Ascii/src/utility/AvrI2c.h ../SSD1306Ascii/src/SSD1306Ascii.h
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.6.364\include/avr/iom328p.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
+
+../SD/src/SD.h:
+
+C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdbool.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\pgmspace.h:
+
+C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h:
+
+C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\ctype.h:
+
+C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h:
+
+C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h:
+
+C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h:
+
+C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h:
+
+C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h:
+
+C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\eeprom.h:
+
+C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h:
+
+C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h:
+
+../SD/src/utility/SdFat.h:
+
+../SD/src/utility/Sd2Card.h:
+
+../SD/src/utility/Sd2PinMap.h:
+
+../SD/src/utility/SdInfo.h:
+
+../SD/src/utility/FatStructs.h:
+
+C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h:
+
+../SD/src/utility/SdFatUtil.h:
+
+../SPI/src/SPI.h:
+
+../Wire/src/Wire.h:
+
+C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h:
+
+../SSD1306Ascii/src/SSD1306Ascii.h:
+
+../SSD1306Ascii/src/SSD1306init.h:
+
+../SSD1306Ascii/src/fonts/allFonts.h:
+
+../SSD1306Ascii/src/fonts/Adafruit5x7.h:
+
+../SSD1306Ascii/src/fonts/font5x7.h:
+
+../SSD1306Ascii/src/fonts/lcd5x7.h:
+
+../SSD1306Ascii/src/fonts/Stang5x7.h:
+
+../SSD1306Ascii/src/fonts/X11fixed7x14.h:
+
+../SSD1306Ascii/src/fonts/X11fixed7x14B.h:
+
+../SSD1306Ascii/src/fonts/ZevvPeep8x16.h:
+
+../SSD1306Ascii/src/fonts/System5x7.h:
+
+../SSD1306Ascii/src/fonts/SystemFont5x7.h:
+
+../SSD1306Ascii/src/fonts/Iain5x7.h:
+
+../SSD1306Ascii/src/fonts/Arial14.h:
+
+../SSD1306Ascii/src/fonts/Arial_bold_14.h:
+
+../SSD1306Ascii/src/fonts/Corsiva_12.h:
+
+../SSD1306Ascii/src/fonts/Verdana_digits_24.h:
+
+../SSD1306Ascii/src/fonts/Callibri10.h:
+
+../SSD1306Ascii/src/fonts/Callibri11.h:
+
+../SSD1306Ascii/src/fonts/Callibri11_bold.h:
+
+../SSD1306Ascii/src/fonts/Callibri11_italic.h:
+
+../SSD1306Ascii/src/fonts/Callibri14.h:
+
+../SSD1306Ascii/src/fonts/Callibri15.h:
+
+../SSD1306Ascii/src/fonts/Cooper19.h:
+
+../SSD1306Ascii/src/fonts/Cooper21.h:
+
+../SSD1306Ascii/src/fonts/Cooper26.h:
+
+../SSD1306Ascii/src/fonts/TimesNewRoman13.h:
+
+../SSD1306Ascii/src/fonts/TimesNewRoman13_italic.h:
+
+../SSD1306Ascii/src/fonts/TimesNewRoman16.h:
+
+../SSD1306Ascii/src/fonts/TimesNewRoman16_bold.h:
+
+../SSD1306Ascii/src/fonts/TimesNewRoman16_italic.h:
+
+../SSD1306Ascii/src/fonts/Verdana12.h:
+
+../SSD1306Ascii/src/fonts/Verdana12_bold.h:
+
+../SSD1306Ascii/src/fonts/Verdana12_italic.h:
+
+../SSD1306Ascii/src/fonts/Roosewood22.h:
+
+../SSD1306Ascii/src/fonts/Roosewood26.h:
+
+../SSD1306Ascii/src/fonts/fixednums7x15.h:
+
+../SSD1306Ascii/src/fonts/fixednums8x16.h:
+
+../SSD1306Ascii/src/fonts/fixednums15x31.h:
+
+../SSD1306Ascii/src/fonts/CalBlk36.h:
+
+../SSD1306Ascii/src/fonts/CalLite24.h:
+
+../SSD1306Ascii/src/fonts/lcdnums12x16.h:
+
+../SSD1306Ascii/src/fonts/lcdnums14x24.h:
+
+../SSD1306Ascii/src/fonts/fixed_bold10x15.h:
+
+../SSD1306Ascii/src/fonts/Wendy3x5.h:
+
+../SSD1306Ascii/src/fonts/newbasic3x5.h:
+
+../SSD1306Ascii/src/fonts/font8x8.h:
+
+../SSD1306Ascii/src/fonts/cp437font8x8.h:
+
+../SSD1306Ascii/src/fonts/utf8font10x16.h:
+
+../SSD1306Ascii/src/SSD1306AsciiAvrI2c.h:
+
+../SSD1306Ascii/src/utility/AvrI2c.h:
+
+../SSD1306Ascii/src/SSD1306Ascii.h:
